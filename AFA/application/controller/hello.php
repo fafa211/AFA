5a3a40a5bb23c3bc9f2e8ef2c6cf9c7a3947bfe0
zhengshufa <22575353@qq.com>
2015-10-10 16:57:24 +0800

    新增sql build类，用于生成SQL语句，防止SQL注入

--- AFA/application/controller/hello.php
+++ AFA/application/controller/hello.php
@@ -30,4 +30,65 @@ class Hello_Controller extends Controller{
         
         //$this->template->render();
     }
+    
+    /**
+     * view test template
+     */
+    public function template($name = ''){
+        $view = new View('view');
+        $view->name = $name?$name:'it is view function';
+        $view->value = 'Github update!';
+    
+        $this->template->content = $view;
+        $this->template->render();
+    }
+    
+    /**
+     * model test
+     */
+    public function model(){
+        //添加新用户
+//         $user = new User_Model();
+//         $user->account = 'admin';
+//         $user->passwd = md5($user->account.'123456');
+//         $user->regtime = date('Y-m-d H:i:s');
+//         $user->save();
+        $user = new User_Model(1);
+        echo $user->account;
+        echo '<br />';
+        
+        $user2 = new User_Model();
+        $user2->get('admin');
+        
+        echo $user2->passwd;
+        echo '<br />';
+        
+    }
+    
+    /**
+     * sql build test
+     */
+    public function sql(){
+        
+        $sql = new sql('UPDATE');
+        
+        $arr = array('passwd'=>md5('fafa654321'));
+        echo $sql->table('user')->set($arr)->where(array('account'=>'fafa'));
+        echo '<br />';
+        echo sql::select('account,passwd,regtime,id', 'user', array('id'=>1))->limit(0,2)->groupby('account')->orderby('id desc');
+        echo '<br />';
+        echo sql::update()->table('user')->set(array('regtime'=>date('Y-m-d H:i:s')))->where(array('id'=>1));
+        echo '<br />';
+        echo sql::select('*', 'user');
+        echo '<br />';
+        echo sql::select('*', 'user as u')->in('u.id', array(1,3,'rtew'))->innerjoin('user_textinfo as ut', 'u.id = ut.user_id');
+        echo '<br />';
+        echo sql::select('*', 'user as u')->in('u.id', array(1))->innerjoin('user_textinfo as ut', 'u.id = ut.user_id')->limit(1)->or_c('id','=',3)->orderby('id desc');
+        
+        echo '<br />';
+        echo sql::select('*', 'user as u')->where('id','>',1)->innerjoin('user_textinfo as ut', 'u.id = ut.user_id')->limit(1)->or_c('id','=',3)->and_c(array('id'=>1))->orderby('id desc');
+        
+        
+        
+    }
 }
--- AFA/system/class/db.php
+++ AFA/system/class/db.php
@@ -2,7 +2,7 @@
 
 /**
  * 数据库操作类 db 使用->PDO 接口
- * @author    alfa
+ * @author    郑书发
  * @version    1.0
  */
 
@@ -186,4 +186,323 @@ class db
        }
        
        
+}
+
+
+/*********************************************************************************
+ * sql build 类， 不支持子查询
+ * $Author:郑书发
+ * $Dtime:2015-10-10
+ ***********************************************************************************/
+class sql {
+    //SQL语句
+    private $sql = '';
+    //SQL类型
+    private $type = '';
+    private $filed = '';
+    private $where = '';
+    private $table = '';
+    private $groupby = '';
+    private $orerby = '';
+    private $limit = '';
+    private $set = '';
+    private $innerarr = array();
+    private $leftarr = array();
+    private $rightarr = array();
+    
+    /**
+     * 
+     * @param string $type: 类型总共三个 SELECT,INSERT,UPDATE
+     */
+    public function __construct($type = 'SELECT'){
+        $this->type = strtoupper($type); 
+    }
+    
+    /**
+     * 组装读取的字段
+     * @param string|array $filed
+     * @param string $from :table name
+     * @param array $where array('key' => 'val')
+     * @return sqlbuild object
+     */
+    public static function select($filed = '', $from = '', $where = array()){
+        $_this = new self('SELECT');
+        $_this->filed($filed);
+        $_this->from($from);
+        $_this->where($where);
+        return $_this;
+    }
+    
+    /**
+     * 插入语句
+     * @param array $val array('key' => 'val')
+     * @param string $table
+     * @return sqlbuild object
+     */
+    public static function insert($val = array(), $table = ''){
+        $_this = new self('INSERT');
+        $_this->set($val);
+        return $_this;
+    }
+    
+    /**
+     * 更新语句
+     * @param array $val array('key' => 'val')
+     * @param string $table
+     * @param array $where array('key' => 'val')
+     * @return sqlbuild object
+     */
+    public static function update($val = array(), $table = '', $where = array()){
+        $_this = new self('UPDATE');
+        $_this->set($val);
+        $_this->table($table);
+        $_this->where($where);
+        return $_this;
+    }
+    
+    /**
+     * SQL组装-组装AND符号的WHERE语句
+     * 返回：WHERE a = 'a' AND b = 'b'
+     * @param string|array $val array('key' => 'val')
+     * @param string $flag : = > < >= <=
+     * @param string|int $value
+     * @param string $connect:链接关键词 ' AND ' 或者 ' OR '
+     * @return string
+     */
+    public function where($arr, $flag = FALSE, $value = '', $connect = ' AND '){
+        if (!is_array($arr)) {
+            if ($flag){
+                if($this->where){
+                    $this->where .= $connect.$arr.$flag.self::escape($value);
+                }else{
+                    $this->where = $arr.$flag.self::escape($value);
+                }
+            }else{
+                if($this->where){
+                    $this->where .= $connect.$arr;
+                }else{
+                    $this->where = $arr;
+                }
+            }
+        }else {
+            $temp = array();
+            foreach ($arr as $k => $v) {
+                $temp[] = "$k=" . self::escape($v) . "";
+            }
+            if ($this->where){
+                $this->where .= $connect.implode(' AND ', $temp);
+            }else{
+                $this->where = implode(' AND ', $temp);
+            }
+        }
+        return $this;
+    }
+    
+    /**
+     * where 条件里的 AND 链接，
+     * 与where功能相同，实际调用 $this->where($arr, $flag, $value)
+     * @param string|array $val array('key' => 'val')
+     * @param string $flag : = > < >= <=
+     * @param string|int $value
+     * @return $this 对象
+     */
+    public function and_c($arr, $flag = '', $value = ''){
+        return $this->where($arr, $flag, $value);
+    }
+    
+    /**
+     * where 条件里的 OR 链接，
+     * 实际调用 $this->where($arr, $flag, $value, ' OR ')
+     * @param string|array $val array('key' => 'val')
+     * @param string $flag : = > < >= <=
+     * @param string|int $value
+     * @return $this 对象
+     */
+    public function or_c($arr, $flag = '', $value = ''){
+        return $this->where($arr, $flag, $value, ' OR ');
+    }
+    
+    /**
+     * 设置查询|插入|更新|删除数据表
+     * @param string 数据表名
+     * @return $this 对象
+     */
+    public function from($table){
+        $this->table = $table;
+        return $this;
+    }
+    
+    /**
+     * 设置查询|插入|更新|删除数据表
+     * 内部实际调用 $this->from($table)
+     * @param string 数据表名
+     * @return $this 对象
+     */
+    public function table($table){
+        return $this->from($table);
+    }
+    
+    /**
+     * 设置查询的字段
+     * @param string|array $filed 字段：多个字段用逗号(,)分隔，或者把字段放在一维数组中
+     * @return $this object
+     */
+    public function filed($filed = '*'){
+        if (is_array($filed)){
+            $this->filed = join(',', $filed);
+        }else {
+            $this->filed = $filed;
+        }
+        return $this;
+    }
+    
+    /**
+     * 插入|更新时设置字段值
+     * @param array $val array('key' => 'val')
+     * @return $this object
+     */
+    public function set($arr){
+        if (!is_array($arr) || empty($arr)) return $this;
+        $temp = array();
+        foreach ($arr as $k => $v) {
+            $temp[] = "`$k`=".self::escape($v)."";
+        }
+        $this->set = implode(',', $temp);
+        return $this;
+    }
+    
+    /**
+     * 设置inner join 表名与关联字段关系
+     * @param string $table:表名
+     * @param string $condition:关联关系，如 user.id=userinfo.user_id
+     * @return $this object
+     */
+    public function innerjoin($table, $condition){
+        $this->innerarr[] = array('table'=>$table, 'condition'=>$condition);
+        return $this;
+    }
+    
+    /**
+     * 设置left join 表名与关联字段关系
+     * @param string $table:表名
+     * @param string $condition:关联关系，如 user.id=userinfo.user_id
+     * @return $this object
+     */
+    public function leftjoin($table, $condition){
+        $this->leftarr[] = array('table'=>$table, 'condition'=>$condition);
+        return $this;
+    }
+    
+    /**
+     * 设置right join 表名与关联字段关系
+     * @param string $table:表名
+     * @param string $condition:关联关系，如 user.id=userinfo.user_id
+     * @return $this object
+     */
+    public function rightjoin($table, $condition){
+        $this->rightarr[] = array('table'=>$table, 'condition'=>$condition);
+        return $this;
+    }
+    
+    /**
+     * 设置分组字段
+     * @param string $groupby:分组字段，多个字段用逗号(,)分隔
+     * @return $this object
+     */
+    public function groupby($groupby){
+        $this->groupby = $groupby;
+        return $this;
+    }
+    
+    /**
+     * 设置排序字段
+     * @param string $orderby:比如 id desc
+     * @return $this object
+     */
+    public function orderby($orderby){
+        $this->orerby = $orderby;
+        return $this;
+    }
+    
+    /**
+     * 设置排序字段
+     * @param int $offset:起始位置
+     * @param int $num:结果条数
+     * @return $this object
+     */
+    public function limit($offset = 0, $num = 0){
+        $this->limit = $offset.($num?','.$num:'');
+        return $this;
+    }
+    
+    /**
+     * 设置排序字段
+     * @param string $filed:字段
+     * @param array $arr:一维数组
+     * @return $this object
+     */
+    public function in($filed, $arr){
+        foreach ($arr as $k=>$v){
+            $arr[$k] = self::escape($v);
+        }
+        return $this->where($filed.' IN ('.join(',', $arr).')');
+    }
+    
+    /**
+     * 防止SQL注入方法
+     * @param string $val:字符串
+     * @return string
+     */
+    public static function escape($val){
+        if (is_int($val)) return $val;
+        return "'".(get_magic_quotes_gpc()?$val:addslashes($val))."'";
+    }
+    
+    /**
+     * 把对象自动解析成SQL语句
+     * @return string : sql
+     */
+    public function __toString(){
+        if ($this->type == 'INSERT'){
+            $this->sql = $this->type.' INTO `'.$this->table.'` SET '.$this->set;
+        }else if($this->type == 'UPDATE'){
+            $this->sql = $this->type.' `'.$this->table.'` SET '.$this->set;
+            if ($this->where) {
+                $this->sql .= ' WHERE '.$this->where;
+            }
+        }else if ($this->type == 'SELECT'){
+            $this->sql = $this->type.' '.$this->filed.' FROM '.$this->table;
+            foreach ($this->innerarr as $v){
+                $this->sql .= ' INNER JOIN '.$v['table'].' ON '.$v['condition'];
+            }
+            foreach ($this->leftarr as $v){
+                $this->sql .= ' LEFT JOIN '.$v['table'].' ON '.$v['condition'];
+            }
+            foreach ($this->rightarr as $v){
+                $this->sql .= ' RIGHT JOIN '.$v['table'].' ON '.$v['condition'];
+            }
+            if ($this->where){
+                $this->sql .= ' WHERE '.$this->where;
+            }
+            if ($this->groupby){
+                $this->sql .= ' GROUP BY '.$this->groupby;
+            }
+            if ($this->orerby){
+                $this->sql .= ' ORDER BY '.$this->orerby;
+            }
+            if ($this->limit){
+                $this->sql .= ' LIMIT '.$this->limit;
+            }
+            
+        }
+        return $this->sql;
+    }
+    
+    /**
+     * 把对象解析成SQL语句
+     * @return string : sql
+     */
+    public function render(){
+        return $this->__toString();
+    }
 }
