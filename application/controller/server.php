dfa15e7536ab5c11f73854c17fabe6857875be14
zhengshufa <22575353@qq.com>
2017-04-13 15:02:02 +0800

    新增token有效期验证

--- application/controller/server.php
+++ application/controller/server.php
@@ -17,6 +17,46 @@ class Server_Controller extends Controller
     protected $ret = array('errNum'=>0, 'errMsg'=>'success', 'retData'=>array());
 
     /**
+     * @var int $account_id 授权帐号ID, 通过key或token解密获得
+     */
+    protected $account_id = 0;
+
+    /**
+     * @var array 允许使用token进行授权访问的方法
+     */
+    protected $use_token = array();
+
+    /**
+     * @var string $token 临时授权码, 可通过getoken接口获得
+     */
+    protected $token = '';
+
+    /**
+     * @var int $token_time token生成时间戳
+     */
+    protected $token_time = 0;
+
+    /**
+     * @var int $token_valid_time token有效时间,默认600秒
+     */
+    protected $token_valid_time = 600;
+
+    /**
+     * @var int 当前时间戳
+     */
+    protected $current_time = 0;
+
+    /**
+     * @var string 客户IP
+     */
+    protected $client_ip = '';
+
+    /**
+     * @var string 请求访问唯一ID, 由系统自动生成, 每次请求都不一样
+     */
+    protected $uniqid = '';
+
+    /**
      * @var string 记录服务调用日志文件保存路径
      */
     public static $log_path = '';//
@@ -28,14 +68,14 @@ class Server_Controller extends Controller
     public function before(){
 
         //忽略man方法
-        if('man' == $this->request->method)  return;
+        if('man' == $this->request->method)  return true;
 
         //验证是否通过标识
         $verify = false;
         //访问控制key
-        $keyno = input::get('key');
-        //授权帐号ID, 默认值为 0
-        $this->account_id = 0;
+        $keyno = trim(input::get('key'));
+        //初始化当前时间戳
+        $this->current_time = time();
 
         if(!empty($keyno)) {
             //账户key授权验证
@@ -46,7 +86,7 @@ class Server_Controller extends Controller
                 $this->account_id = $keyarr[0];
                 $verify = true;
             }
-        }elseif($token = input::get('token')){
+        }elseif($token = trim(input::get('token'))){
             //token授权验证
             if(is_array($this->use_token) && in_array($this->request->method, $this->use_token)) {
                 //解密token
@@ -54,9 +94,10 @@ class Server_Controller extends Controller
                 if (!empty($str)) {
                     list($this->account_id, $time) = explode(',', $str);
 
-                    if ($this->account_id && $time <= time()) {
+                    if ($this->account_id && ($time + $this->token_valid_time) > $this->current_time) {
                         $this->token = $token;
                         $this->token_time = $time;
+
                         $verify = true;
                     }
                 }
@@ -83,6 +124,13 @@ class Server_Controller extends Controller
         );
         $logContent = F::json_encode($logContent);
         $logs->LogInfo($logContent);
+
+        if($verify === false){
+            $this->ret['errNum'] = "-1";
+            $this->ret['errMsg'] = "token验证失败或已失效!";
+
+            $this->echojson($this->ret);
+        }
 
         return $verify;
 
